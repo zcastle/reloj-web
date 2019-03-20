8219fa2e0d42c3f2b8e9d2eed8773d603528e920
jc <jzcastle@gmail.com>
2019-03-19 22:56:46 -0500

    change format date

--- src/lib/Xls.php
+++ src/lib/Xls.php
@@ -4,6 +4,8 @@ namespace Lib;
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 //use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+//use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class Xls {
@@ -16,14 +18,23 @@ class Xls {
         $sheet->setCellValue('A'.$ln, 'LN');
         $sheet->setCellValue('B'.$ln, 'CODIGO');
         $sheet->setCellValue('C'.$ln, 'EMPLEADO');
-        $sheet->setCellValue('D'.$ln++, 'MARCACION');
+        $sheet->setCellValue('D'.$ln, 'MARCACION');
+        //$sheet->setCellValue('E'.$ln, 'MARCACION');
+        $ln++;
 
         $i = 1;
         foreach($rows AS $row){
-            $sheet->setCellValue('A'.$ln, $i++);
+            $sheet->setCellValue('A'.$ln, $i);
             $sheet->setCellValue('B'.$ln, $row->codigo);
             $sheet->setCellValue('C'.$ln, $row->empleado);
-            $sheet->setCellValue('D'.$ln++, $row->fecha_hora);
+            $sheet->setCellValue('D'.$ln, Date::PHPToExcel($row->fecha_hora));
+            //$sheet->setCellValue('D'.$ln, Date::PHPToExcel(gmmktime(0, 0, 0, date('m'), date('d'), date('Y'))));
+            //NumberFormat::FORMAT_DATE_DATETIME
+            $sheet->getStyle('D'.$ln)->getNumberFormat()->setFormatCode("dd/mm/yyyy h:mm:ss");
+            //$sheet->setCellValue('E'.$ln, $row->fecha_hora);
+
+            $i++;
+            $ln++;
         }
 
         return IOFactory::createWriter($spreadsheet, 'Xlsx');
